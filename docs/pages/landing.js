58896b6ebf8ecc00b4fb99709004383f6e64b76a
ashusharmasigdev <ashusharma.sigmadev@gmail.com>
2022-05-03 12:26:26 +0530

    node parsed & somehow basic completed

--- docs/pages/landing.js
+++ docs/pages/landing.js
@@ -4,7 +4,7 @@ class LandingPage extends NiftComponent
     render = () => {
         return (`
         
-        <NiftParent>
+        <NiftParent x="0" y="0" w="300" h="30" background="blue">
             <Heading fontsize=${80}>I am Rendered by Nift*UI.</Heading>
         </NiftParent>
         
--- niftui/js/niftui.js
+++ niftui/js/niftui.js
@@ -1,5 +1,6 @@
-const NIFT_UI_SUPPORT_VERSION = '0.1';
+const NIFT_UI_SUPPORT_VERSION = 'ZERO_ONE';
 const NIFT_UI_VERSION = '0.1';
+const NIFT_CANVAS_ID = "niftCanvas"+NIFT_UI_SUPPORT_VERSION;
 
 const getCurrentRoute = () => {
     return '/';
@@ -15,8 +16,11 @@ class NiftInstance
         this.parent = parent;
         this.routes = false;
         this.niftarea = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        this.niftarea.setAttribute("niftver", NIFT_UI_VERSION);
-        this.niftarea.setAttribute("niftsupport", NIFT_UI_SUPPORT_VERSION);
+        this.niftarea.setAttribute("data-niftver", NIFT_UI_VERSION);
+        this.niftarea.setAttribute("data-niftsupport", NIFT_UI_SUPPORT_VERSION);
+        this.niftarea.setAttribute("id", NIFT_CANVAS_ID);
+        this.niftarea.setAttribute("x", 0);
+        this.niftarea.setAttribute("y", 0);
         this.parent.appendChild(this.niftarea);
     }
 
@@ -25,8 +29,9 @@ class NiftInstance
     }
 
     renderElement = (element = new NiftComponent) => {
-        // this.niftarea.appendChild(element.render());
-        this.niftarea.innerHTML = (element.innerRender());
+        // this.niftarea.appendChild(element.self);
+        this.niftarea.appendChild(element.innerRender());
+        // this.niftarea.innerHTML = (element.innerRender());
     }
 
     compile = () => {
@@ -60,6 +65,17 @@ class NiftRoute
     }
 }
 
+class NiftProperty
+{
+
+    constructor(name, value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+}
+
 class NiftComponent
 {
 
@@ -69,7 +85,8 @@ class NiftComponent
             parent = document.getElementById("niftBody");
 
         this.parent = parent;
-        // this.self = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        this.self = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        this.props = [];
         this.selfDOM = false;
     }
 
@@ -82,10 +99,71 @@ class NiftComponent
     render = () => {}
 
     innerRender = () => {
-        // let dom = new DOMParser();
-        // this.selfDOM = dom.parseFromString(this.render, "text/html");
-        // console.log(this.selfDOM)
-        return this.render();
+
+        let dom = new DOMParser();
+        this.selfDOM = dom.parseFromString(this.render(), "text/html").all[3];
+
+        for (const key in this.selfDOM.attributes) {
+            if (Object.hasOwnProperty.call(this.selfDOM.attributes, key)) {
+                const element = this.selfDOM.attributes[key];
+                this.props.push(new NiftProperty(element.nodeName, element.nodeValue));
+            }
+        }
+
+        // Set Points
+        let x = 0, y = 0, w = x+0, h = y+0;
+        this.props.map(prop => {
+            if(prop.name == 'x')
+            {
+                x = parseInt(prop.value);
+            }
+            else if(prop.name == 'y')
+            {
+                y = parseInt(prop.value);
+            }
+            else if(prop.name == 'w')
+            {
+                w = parseInt(prop.value);
+            }
+            else if(prop.name == 'h')
+            {
+                h = parseInt(prop.value);
+            }
+            else if(prop.name == 'background')
+            {
+                this.self.setAttribute("fill", prop.value);
+            }
+        });
+
+        w += x;
+        h += y;
+
+        let points = [
+            {
+                x: x,
+                y: y
+            },
+            {
+                x: w,
+                y: y
+            },
+            {
+                x: w,
+                y: h
+            },
+            {
+                x: x,
+                y: h
+            }
+        ];
+
+        let pointsString = points.reduce((string, point) => {
+            return string + parseInt(point.x) + "," + parseInt(point.y) + " ";
+        }, "");
+
+        this.self.setAttribute("points", pointsString);
+
+        return this.self;
     }
 
 };
